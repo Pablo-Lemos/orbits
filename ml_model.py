Added change of senders/receivers and fixed normalization

ml_model.py
@@ -16,17 +16,18 @@ class Normalize_gn(tf.keras.layers.Layer):
         maxs_phi = tf.constant([np.pi])
         mins_theta = tf.constant([0.])
         mins_phi = tf.constant([-np.pi])
-        maxs_m = tf.reduce_max(inputs[:,3:5], axis = 0)
-        mins_m = tf.reduce_min(inputs[:,3:5], axis = 0)
+        maxs_m = tf.constant([12.])
+        mins_m = tf.constant([-12.])
+        #maxs_m = tf.reduce_max(inputs[:,3:5])
+        #mins_m = tf.reduce_min(inputs[:,3:5])
+        #maxs_m = tf.reshape(maxs_m, shape=(1,))
+        #mins_m = tf.reshape(mins_m, shape=(1,))
 
-        maxs = tf.concat([maxs_r, maxs_theta, maxs_phi, maxs_m], axis = -1)
-        mins = tf.concat([mins_r, mins_theta, mins_phi, mins_m], axis = -1)
+        maxs = tf.concat([maxs_r, maxs_theta, maxs_phi, maxs_m, maxs_m], axis = -1)
+        mins = tf.concat([mins_r, mins_theta, mins_phi, mins_m, mins_m], axis = -1)
 
         X = (inputs - mins)/(maxs-mins)
         outputs = 2*X - 1
-        #outputs = tf.concat([outputs[:,:-1], temp], axis = -1)
-        #print('norm before =', inputs)
-        #print('norm after =', outputs)
     
         return outputs
 
@@ -99,7 +100,7 @@ class LearnForces(tf.keras.Model):
         self.logm_planets = tf.Variable(
             initial_value=logm_init(shape=(self.nplanets,), dtype="float32"),
             trainable=True,
-            constraint=lambda z: tf.clip_by_value(z, -20, 12)
+            constraint=lambda z: tf.clip_by_value(z, -12, 12)
         )
         
         norm_layer = Normalize_gn()
@@ -145,6 +146,7 @@ class LearnForces(tf.keras.Model):
             
         #a = tf.constant([np.log10(5.522376708530351)], dtype = tf.float32)
         #lm = tf.concat([a, lm], axis=0)
+        lm = tf.clip_by_value(lm, -12, 12, name=None)
         self.logmasses = lm
 
         nodes_g = tf.concat([lm]*ntime, axis = 0)
@@ -155,10 +157,16 @@ class LearnForces(tf.keras.Model):
                                                              self.nplanets, 
                                                              self.nedges)
         
+        if training == True:
+            senders_g, receivers_g, signs = shuffle_senders_receivers(senders_g, 
+                                                                  receivers_g)
+        else:
+            signs = np.ones(len(senders_g))
+        
         # Create graph
         graph_dict = { 
           "nodes": nodes_g,
-          "edges": cartesian_to_spherical_coordinates(D), 
+          "edges": cartesian_to_spherical_coordinates(D*signs[:,np.newaxis]), 
           "receivers": receivers_g, 
           "senders": senders_g ,
           #"globals": self.logG
@@ -177,7 +185,7 @@ class LearnForces(tf.keras.Model):
         a = self.get_acceleration(f, g)
         #print('a =', a)
         if extract == True: 
-            f = tf.reshape(g.edges, shape=[-1, self.nedges, 3]).numpy()
+            f = tf.reshape(g.edges*signs[:,np.newaxis], shape=[-1, self.nedges, 3]).numpy()
             a = tf.reshape(a, shape=[-1, self.nplanets, 3]).numpy()
             return a, f
         else: 